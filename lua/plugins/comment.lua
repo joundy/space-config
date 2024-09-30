92f2498de9f02f704138c5db20d5396588c63a48
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2024-09-30 23:17:26 +0700

    update: comment plugin

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,40 @@
+return {
+	dependencies = {
+		{
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			branch = "main",
+			commit = "44fd461b879d80a21d5041f312f070f22551c0bc",
+			lazy = true,
+			config = function()
+				require("ts_context_commentstring").setup({
+					enable_autocmd = false,
+				})
+			end,
+		},
+	},
+	"numToStr/Comment.nvim",
+	branch = "master",
+	commit = "e30b7f2008e52442154b66f7c519bfd2f1e32acb",
+	lazy = false,
+	config = function()
+		local comment = require("Comment")
+
+		comment.setup({
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+		})
+
+		-- Keymaps
+		local keymap = vim.keymap.set
+		local opts = { noremap = true, silent = true }
+
+		keymap("n", "<Leader>/", function()
+			require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
+		end, opts)
+		keymap("v", "<Leader>/", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", opts)
+
+		keymap("n", "<Leader>?", function()
+			require("Comment.api").toggle.blockwise.count(vim.v.count > 0 and vim.v.count or 1)
+		end, opts)
+		keymap("v", "<Leader>?", "<esc><cmd>lua require('Comment.api').toggle.blockwise(vim.fn.visualmode())<cr>", opts)
+	end,
+}
