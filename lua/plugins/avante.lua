e7a62f2a49211c301a8c4dac2b3ed568a27ce91a
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2024-10-01 00:14:26 +0700

    add: avante

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,8 @@
+return {
+  -- "yetone/avante.nvim",
+  -- event = "VeryLazy",
+  -- branch = "main",
+  -- commit = "0705234991d03170a72582085dc508600a03a779",
+  -- lazy = false,
+  -- build = "make",
+}
