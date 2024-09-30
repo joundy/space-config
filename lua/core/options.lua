bea97f3027502c4aad204b9850b6ae673480d862
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2024-09-30 20:11:10 +0700

    feat: add heirline statusline with git, LSP, and diagnostics info

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,3 +3,4 @@ local opt = vim.opt
 -- Line Numbers
 opt.relativenumber = true
 opt.number = true
+opt.scrolloff = 8
--- lua/plugins/_deps.lua
+++ lua/plugins/_deps.lua
@@ -1,13 +1,20 @@
 return {
 	{
-		priority = 1000,
+		priority = 20000,
 		"nvim-tree/nvim-web-devicons",
 		branch = "master",
 		commit = "1e5a2a796a88b5d696acb07dd5c285b13d13da21",
 		lazy = false,
 	},
 	{
-		priority = 1000,
+		priority = 15000,
+		"nvim-lualine/lualine.nvim",
+		branch = "master",
+		commit = "b431d228b7bbcdaea818bdc3e25b8cdbe861f056",
+		lazy = false,
+	},
+	{
+		priority = 20000,
 		"nvim-lua/plenary.nvim",
 		tag = "v0.1.4",
 		lazy = false,
