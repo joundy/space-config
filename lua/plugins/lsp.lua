14b3ac005ddad017f2a23d9a0e1506d2bb2b53d5
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2024-09-30 23:51:58 +0700

    refactor: improve code formatting and update LSP configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,15 +4,26 @@ return {
 	lazy = false,
 	priority = 2000,
 	keys = {
-		{ "gd", vim.lsp.buf.definition, desc = "Go to definition", mode = "n" },
-		{ "gD", vim.lsp.buf.declaration, desc = "Go to declaration", mode = "n" },
-		{ "<space>lr", vim.lsp.buf.rename, desc = "Rename symbol", mode = "n" },
-		{ "K", vim.lsp.buf.hover, desc = "Show hover information", mode = "n" },
-		{ "<leader>ld", "<cmd>lua vim.diagnostic.open_float()<cr>", desc = "Open diagnostic float", mode = "n" },
-		{ "<Leader>lR", function() require("telescope.builtin").lsp_references() end, desc = "List references", mode = "n" },
-		{ "<leader>lD", function() require("telescope.builtin").diagnostics() end, desc = "List diagnostics", mode = "n" },
+		{ "gd",         vim.lsp.buf.definition,                     desc = "Go to definition",       mode = "n" },
+		{ "gD",         vim.lsp.buf.declaration,                    desc = "Go to declaration",      mode = "n" },
+		{ "<space>lr",  vim.lsp.buf.rename,                         desc = "Rename symbol",          mode = "n" },
+		{ "K",          vim.lsp.buf.hover,                          desc = "Show hover information", mode = "n" },
+		{ "<leader>ld", "<cmd>lua vim.diagnostic.open_float()<cr>", desc = "Open diagnostic float",  mode = "n" },
+		{
+			"<Leader>lR",
+			function()
+				require("telescope.builtin").lsp_references()
+			end,
+			desc = "List references",
+			mode = "n",
+		},
+		{
+			"<leader>lD",
+			function()
+				require("telescope.builtin").diagnostics()
+			end,
+			desc = "List diagnostics",
+			mode = "n",
+		},
 	},
-	config = function()
-		-- Any additional configuration can go here
-	end,
 }
