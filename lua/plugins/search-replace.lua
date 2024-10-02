38c25dbbc0e46d5ea1542f2e39e766b9810e0e96
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2024-10-02 19:24:53 +0700

    updates

---
+++ lua/plugins/search-replace.lua
@@ -0,0 +1,18 @@
+return {
+	{
+
+		"dyng/ctrlsf.vim",
+		version = "v2.6.0",
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			vim.g.ctrlsf_ignore_dir = { '.gitignore', '.git', 'node_modules', 'target' }
+		end,
+
+	},
+	{
+		"mg979/vim-visual-multi",
+		branch = "master",
+		commit = "a6975e7c1ee157615bbc80fc25e4392f71c344d4",
+		event = { "BufReadPost", "BufNewFile" },
+	}
+}
